a623b5beb35f1b4cc30cae737808d98a6b36524e
[Ayman Emad] <aymanemad110@gmail.com>
2024-11-23 20:46:02 +0200

    Second commit

--- src/components/EntryData.jsx
+++ src/components/EntryData.jsx
@@ -1,6 +1,6 @@
-import JAPAN from "/JAPAN.jpg";
-import AUSTRALIA from "/AUSTRALIA.jpg";
-import NORWAY from "/NORWAY.jpg";
+import JAPAN from "../assets/JAPAN.jpg";
+import AUSTRALIA from "../assets/AUSTRALIA.jpg";
+import NORWAY from "../assets/NORWAY.jpg";
 
 const entryData = [
   {
