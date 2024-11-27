b60a5f70aff81e387c50dfa49abd77a910abb67d
[Ayman Emad] <aymanemad110@gmail.com>
2024-11-27 19:44:50 +0200

    Fourth commit

--- src/components/EntryData.jsx
+++ src/components/EntryData.jsx
@@ -4,8 +4,8 @@ import norway from "/src/assets/norway.jpg";
 
 const entryData = [
   {
-    id: 1,
-    locationImg: { japan },
+    id: 111,
+    locationImg: japan,
     country: "JAPAN",
     googleMapsLink:
       "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
@@ -15,8 +15,8 @@ const entryData = [
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
   },
   {
-    id: 2,
-    locationImg: { australia },
+    id: 222,
+    locationImg: australia,
     country: "AUSTRALIA",
     googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
     locationName: "Sydney Opera House",
@@ -25,8 +25,8 @@ const entryData = [
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
   },
   {
-    id: 3,
-    locationImg: { norway },
+    id: 333,
+    locationImg: norway,
     country: "NORWAY",
     googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
     locationName: "Geirangerfjord",
